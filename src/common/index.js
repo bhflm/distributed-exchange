a38029fe71a80e9b076be2a6a78b79f687b10781
germi <germanbonin@gmail.com>
2024-02-05 11:09:08 -0300

    Add: better logger, order class, and baseline for orderbook methods

--- src/common/index.js
+++ src/common/index.js
@@ -1,5 +1,14 @@
 
 // Define request object like 
+export function createEnum(values) {
+  const enumObject = {};
+  for (const val of values) {
+    enumObject[val] = val;
+  }
+  return Object.freeze(enumObject);
+};
+
+
 
 const requestConfig = {
   type,
@@ -9,5 +18,5 @@ const requestConfig = {
 
 
 module.exports = {
-  
+
 };
--- src/orderbook/index.js
+++ src/orderbook/index.js
@@ -1,19 +1,66 @@
+import { createEnum } from "../common";
+
+/**
+ * Makes an enum of type
+ * enum {
+ *  SELL,
+ *  BUY
+ * }
+ */
+const OrderType = createEnum(['SELL', 'BUY']);
+
+/**
+ * Order is meant for each instance of a new order
+ */
+class Order { 
+  constructor(type, asset, amount) {
+    if (OrderType[type] === undefined) {
+      throw new Error('Need a valid Order Type');
+    };
+
+    if (amount <= 0) {
+      throw new Error('Amount needs to be positive');
+    }
+
+    this.id = Math.random(); // uid ?
+
+
+    this.type = type; // this should be orderType
+    this.asset = asset;
+    this.amount = amount;
+  };
+};
+
+/**
+ * Orderbook keeps in charge of order functionalities (buy, sell, get?), tracking all orders and ordermatching
+ */
 class OrderBook {
   
   constructor (){ 
     this.orders = new Array();
   }
 
-
-  // WIP
   addOrder(order) {
     console.log('adding order: ', order);
+    this.orders.push(order);
     // do stuff;
     return;
+  }
+
+  matchOrder(order){
+    // @@TODO;
+    return null;
+  };
+
+  getOrder(order) {
+    // @@ TODO;
+
+    return null;
   }
   
 };
 
 module.exports = {
+  Order,
   OrderBook,
 }
